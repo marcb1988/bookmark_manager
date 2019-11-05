543d56e0035b5958d07c37fa0383d297c17215b8
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-05 14:16:13 +0000

    brought up to date with previous partner

--- spec/features/ viewing_bookmarks_spec.rb
+++ spec/features/ viewing_bookmarks_spec.rb
@@ -1,11 +1,6 @@
 require "./app.rb"
 
 feature "Viewing Bookmarks" do
-  scenario "visiting the index page" do
-    visit ("/")
-    expect(page).to have_content "Bookmark Manager"
-  end
-
   scenario "allows you to view the bookmarks" do
     visit ("/bookmarks")
     expect(page).to have_content "http://www.google.com"
